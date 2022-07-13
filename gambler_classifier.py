53edde23cbde6577f62aa33392734c1625758c8a
Geofrey Obara Ong'eta <53979444+obara501@users.noreply.github.com>
2022-07-13 03:55:17 +0300

    Update gambler_classifier.py
    Model outputs made more user friendly.

--- gambler_classifier.py
+++ gambler_classifier.py
@@ -6,29 +6,29 @@ with open('gambler_classifier.pickle','rb') as f:
     clf = pickle.load(f)
 
 def main():
-    
-    st.title('Gambler Classifier')
+    st.title('Gambling Disorder Predictor')
 
     t = st.number_input("Hours Played")
     w = st.number_input("Win/Loss Ratio")
     n = st.number_input("Number of Deposits")
 
     if st.button('Predict'):
-        
+
         result = clf.predict([[t,w,n]])
 
         if result == 0:
-            st.success('Category 0')
+            st.success('You were placed in Category 0: Your behaviour does not seem to be destructive.')
         elif result == 1:
-            st.success('Category 1')
+            st.success('You were placed in Category 1: You have a low chance of developing GD.\nPlay thoughtfully and you will be fine. ')
         elif result == 2:
-            st.success('Category 2')
+            st.success('You were placed in Category 2: You have a low to mediate risk of developing GD.\nPlease try to gamble less or spend less wagers. ')
         elif result == 3:
-            st.success('Category 3')
+            st.success('You were placed in Category 3: You have a mediate to high risk of developing GD.\nDo your friends think you gamble too much? Try gambling less and seek professional help.')
         elif result == 4:
-            st.success('Category 4')
+            st.success('You were placed in Category 4: You have been placed on the high risk scale of developing GD.\nWe suggest you stop gambling immediatly and seek professional help.')
         else:
-            st.success('Category 5')
+            st.success('You were placed in Category 5: You are at the highest risk of developig GD.\nWe suggest you stop gambling immediately. Ask people around you to help you curb your urges and seek professional assistance.')
+        
 
 if __name__=='__main__':
     main()
