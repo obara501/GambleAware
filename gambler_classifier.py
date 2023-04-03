3a75ca04e7da4fca55db6dbc1d5253390eb70043
obara501 <geofreyongeta501@gmail.com>
2023-04-03 21:48:31 +0300

    Modify gambler_classifier.py

--- gambler_classifier.py
+++ gambler_classifier.py
@@ -13,6 +13,7 @@ with open('gambler_classify.pickle','rb') as f:
 def main():
     #Display title for the web app.
     st.title('Gamble-Aware')
+    
     st.text('Gamble Aware is a standardised measure of risk behaviour in problem gambling. \nIt is a tool based on research on the common signs and consequences of \nproblematic gambling. Assessing where you or a family member is can help them \nmake informed decisions on how to assist them')
     st.header('How it works')
     st.text('Fill in the form below to self-assess your gambling behaviour over \nthe past 24 hours.The response options attract the following scores.\n\nCategory 0 - Your behaviour does not seem to be destructive.\nCategory 1 - Low chance of developing GD\nCategory 2 - Low to mediate risk of developing GD\nCategory 3 - Mediate to high risk of developing GD\nCategory 4 - High risk scale of developing GD\nCategory 5 - Highest risk of developig GD\n\n*GD - Gambling Disorder\n')
@@ -25,7 +26,7 @@ def main():
     t = st.number_input("Hours Played")
     n = st.number_input("Number of Deposits Made")
     
-    #Catch float division by zero which creates ZeroDivisionError
+    #Catch float division by zero to rid ZeroDivisionError
     try:
         #Calculate Win/Loss ratio(wlr).
         wlr = (float(b)+float(wd))/float(d) 
@@ -34,7 +35,7 @@ def main():
     #Predict and display predictions.
     if st.button('Predict'):  
 
-        #Returns gambler categories from 0 to 5
+        #Return gambler categories
         result = clf.predict([[t,wlr,n]]) 
 
         if result == 0:
